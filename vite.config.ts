98ce69810c8312e351df05e51bfab58d9977ff6d
Freesia <freesia@studiofreesia.com>
2022-05-04 02:20:48 +0900

    エイリアスの対応

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import { createVuePlugin } from 'vite-plugin-vue2';
+import { defineConfig } from "vite";
+import { createVuePlugin } from "vite-plugin-vue2";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": __dirname + "/src",
+    },
+  },
   plugins: [createVuePlugin()],
 });
