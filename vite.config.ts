87add7aa97084d0ccf4eba9da578b8d56016c2d6
Freesia <freesia@studiofreesia.com>
2022-05-04 04:22:33 +0900

    ビルド時の警告対応

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,31 @@ export default defineConfig({
     createVuePlugin(),
     Components({
       resolvers: [VuetifyResolver()],
-      dts: 'src/components.d.ts',
+      dts: "src/components.d.ts",
     }),
   ],
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
+  css: {
+    // sassのバージョン上げれば動くけど、Vuetifyで警告が出る…
+    // preprocessorOptions: {
+    //   sass: { charset: false },
+    //   scss: { charset: false },
+    // },
+    postcss: {
+      plugins: [
+        {
+          postcssPlugin: "internal:charset-removal",
+          AtRule: {
+            charset: (atRule) => {
+              if (atRule.name === "charset") {
+                atRule.remove();
+              }
+            },
+          },
+        },
+      ],
+    },
+  },
 });
